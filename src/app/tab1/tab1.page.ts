f1baaf4d7ee9c01b224a0afefd68720e71871298
Ilham_Benhsine <benhsine.ilham@gmail.com>
2020-08-20 14:08:32 +0200

    Filter tab2(Recherche) working

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,6 @@
 import { Recette } from './../shared/models/Recette';
 import { BaseRecetteService } from './../shared/base-recette.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
 
 @Component({
@@ -8,19 +8,25 @@ import { Subscription } from 'rxjs';
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page implements OnInit{
+export class Tab1Page implements OnInit, OnDestroy{
 
   recettes: Recette[];
   subscription$: Subscription;
 
   constructor(private baseRecetteService: BaseRecetteService) {
-    this.baseRecetteService.getMessageBehaviorSubject().subscribe(recettes => this.recettes = recettes, error => console.log(error),
-    () => console.log('Finished'));
   }
 
   ngOnInit(){
-  //  this.subscription$ = this.baseRecetteService.getMessageBehaviorSubject().subscribe(recettes => this.recettes = recettes,
-  //     error => console.log(error),
-  //     () => console.log('Finished'));
+   this.subscription$ = this.baseRecetteService.getMessageBehaviorSubject().subscribe(recettes => this.recettes = recettes,
+      error => console.log(error),
+      () => console.log('Finished'));
+    }
+
+    ngOnDestroy() {
+      console.log('ngOnDestroy');
+      if (this.subscription$) {
+        this.subscription$.unsubscribe();
+        console.log('this.subscription$.unsubscribe');
+      }
     }
   }
